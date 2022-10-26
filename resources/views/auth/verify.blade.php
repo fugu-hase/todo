5b20051287188381b52ca46c607da409eceecf10
fugu-hase <konyamoanshinshite@gmail.com>
2022-10-26 18:27:52 +0900

    update files

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+image.png
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -17,7 +17,7 @@
                     {{ __('Before proceeding, please check your email for a verification link.') }}
                     {{ __('If you did not receive the email') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
+                        
                         <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
                     </form>
                 </div>
